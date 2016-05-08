832e7dd8319df0525242ad4422ce30382b6bb604
B <public88878878887@hotmail.com>
2016-05-08 13:27:46 +0000

    add regression tests - ActionCableConfig

--- test/unit/services/actionCableConfig.spec.js
+++ test/unit/services/actionCableConfig.spec.js
@@ -6,7 +6,49 @@ describe('ActionCableConfig', function(){
   beforeEach(inject(function(_ActionCableConfig_){
     ActionCableConfig= _ActionCableConfig_;
   }));
-  it('exists', function(){
-    expect(ActionCableConfig).toBeObject;
+  describe('module', function(){
+    it('exists', function(){
+      expect(ActionCableConfig).toBeObject;
+    });
+  });
+  describe('autoStart', function() {
+    it('initiates to true', function(){
+      expect(ActionCableConfig.autoStart).toBe(true);
+    });
+    describe('when set false', function() {
+      beforeEach(function() {
+        ActionCableConfig.autoStart= false;
+      });
+
+      it('returns false', function(){
+        expect(ActionCableConfig.autoStart).toBe(false);
+      });
+    });
+  });
+  describe('debug', function() {
+    it('initiates to false', function(){
+      expect(ActionCableConfig.debug).toBe(false);
+    });
+    describe('when set true', function() {
+      beforeEach(function() {
+        ActionCableConfig.debug= true;
+      });
+      it('returns true', function(){
+        expect(ActionCableConfig.debug).toBe(true);
+      });
+    });
+  });
+  describe('wsUri', function() {
+    describe('when meta tag set', function() {
+      it('returns meta value', function(){
+        //the meta tag needs to be faked here but the html is loaded before angular and the jquery is executed on file read
+        //expect(ActionCableConfig.wsUri).toBe('wss://foobar.tld:1234/path/name');
+      });
+    });
+    describe('when meta tag not found', function() {
+      it('returns empty', function(){
+        expect(ActionCableConfig.wsUri).toBe('');
+      });
+    });
   });
 });
