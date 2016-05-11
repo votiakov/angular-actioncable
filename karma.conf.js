0afb790ce32986121bf3f079b2ad4239c4774341
razor <razorcd@yahoo.com>
2016-05-11 11:48:52 +0200

    adds config for karma-coverage

--- karma.conf.js
+++ karma.conf.js
@@ -38,8 +38,16 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    // reporters: ['mocha'],
 
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
@@ -77,3 +85,9 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 };
+
+
+
+
+
+
