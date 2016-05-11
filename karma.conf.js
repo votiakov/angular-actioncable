35579ebc01ef846f0ad315561cc99ac10af361f7
razor <razorcd@yahoo.com>
2016-05-11 12:29:00 +0200

    adds travis script

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': ['coverage']
     },
 
 
@@ -45,7 +46,7 @@ module.exports = function(config) {
 
     // optionally, configure the reporter
     coverageReporter: {
-      type : 'html',
+      type : 'lcov',
       dir : 'coverage/'
     },
 
