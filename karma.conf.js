8bae46324da6bd61910fd1c337c5c39b0baf15be
razor <razorcd@yahoo.com>
2016-05-11 12:43:22 +0200

    cleanup

--- karma.conf.js
+++ karma.conf.js
@@ -39,9 +39,6 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    // reporters: ['mocha'],
-
-    // coverage reporter generates the coverage
     reporters: ['progress', 'coverage'],
 
     // optionally, configure the reporter
@@ -86,9 +83,3 @@ module.exports = function(config) {
     concurrency: Infinity
   })
 };
-
-
-
-
-
-
