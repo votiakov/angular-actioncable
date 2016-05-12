014aa091d4f1280718ce94c13d91e117ed0e7ef9
B <public88878878887@hotmail.com>
2016-05-12 00:21:59 +0000

    mock ActionCableWebsocket to prevent live connections

--- test/unit/services/actionCableChannel.spec.js
+++ test/unit/services/actionCableChannel.spec.js
@@ -1,11 +1,27 @@
 'use strict';
 
 describe('ActionCableChannel', function(){
-  var ActionCableChannel;
+  var ActionCableChannel,
+      resetActionCableWebsocketMock,
+      ActionCableWebsocket;
   beforeEach(module('ngActionCable'));
-  beforeEach(inject(function(_ActionCableChannel_){
-    ActionCableChannel= _ActionCableChannel_;
-  }));
+  resetActionCableWebsocketMock= function(){
+    ActionCableWebsocket= {
+      connected: function(){},
+      attempt_restart: function(){},
+      currentChannels: null,
+      close: function(){},
+      on_connection_close_callback: function(){},
+      on_connection_open_callback: function(){},
+      subscribe: function(){},
+      unsubscribe: function(){},
+      send: function(){}
+    };
+  };
+  beforeEach(function(){
+    resetActionCableWebsocketMock();
+    module(function($provide) {$provide.value('ActionCableChannel', ActionCableChannel);});
+  });
   it('exists', function(){
     expect(ActionCableChannel).toBeObject;
   });
