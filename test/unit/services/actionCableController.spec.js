148c7e60742cba25b1a838965e8ccf5b28385904
B <public88878878887@hotmail.com>
2016-08-29 22:45:56 +0000

    test after_ping_callback

--- test/unit/services/actionCableController.spec.js
+++ test/unit/services/actionCableController.spec.js
@@ -27,4 +27,30 @@ describe('ActionCableController', function(){
       expect(rootScope.$broadcast).toHaveBeenCalledWith('confirm_subscription:fooBarChannel');
     });
   });
+  describe('ping', function(){
+    var railsPing= {};
+    beforeEach(function(){
+      railsPing= {
+        five: {
+          type: 'ping'
+        },
+        five_beta_3: {
+          identifier: '_ping'
+        }
+      };
+    });
+    describe('Rails ', function(){
+      ['five', 'five_beta_3'].forEach(function(rails_version, index, array){
+        describe(rails_version, function(){
+          it('after_ping_callback', function(){
+            var fired= false;
+            var callback= function(){ fired= true; };
+            ActionCableController.after_ping_callback= callback;
+            ActionCableController.post(railsPing[rails_version]);
+            expect(fired).toBe(true);
+          });
+        });
+      });
+    });
+  });
 });
